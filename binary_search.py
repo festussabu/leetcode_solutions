d257a1fbd38d506db1e6d3dc61096161274baa2a
festussabu <festus.hoopa@gmail.com>
2026-05-25 13:13:28 -0400

    Binary Search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def search(nums: List[int], target: int) -> int:
+    l = 0
+    r = len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            r = mid - 1
+        elif nums[mid] < target:
+            l = mid + 1
+    return -1
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(search(nums, target))
